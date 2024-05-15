ed8e5b0802e25746856a16705524d214d22e623b
Draco112358 <gianni.frittella@gmail.com>
2024-05-16 01:06:28 +0200

    aggiunti parametri xmin,xmax,ymin,ymax,zmin,zmax per il posizionamento del brick.

--- src/renderer/canvas/components/sideBar/components/geometryParams/cubeGeometryParams.tsx
+++ src/renderer/canvas/components/sideBar/components/geometryParams/cubeGeometryParams.tsx
@@ -1,10 +1,158 @@
-import {CubeGeometryAttributes} from "cad-library";
+import {CubeGeometryAttributes, updateTransformationParams} from "cad-library";
 import {FC} from "react";
 import {GeometryParamsGeneralProps} from "./geometryParams";
+import { useDispatch } from "react-redux";
 
 export const CubeGeometryParams: FC<GeometryParamsGeneralProps> = ({entity, updateParams}) => {
+  const dispatch = useDispatch()
     return (
         <>
+            <div className="flex row">
+              <div key="xmin" className="flex">
+                  <span className="text-black w-[40%] text-left text-xs">Xmin</span>
+                  <div className="flex mb-[5px]">
+                      <input key="xmin"
+                            type="number"
+                            max={entity.transformationParams.position[0] + (entity.geometryAttributes as CubeGeometryAttributes).width/2}
+                            step="0.1"
+                            className="border border-black rounded shadow px-1 w-[50%] text-black text-left text-xs"
+                            autoComplete="off"
+                            value={entity.transformationParams.position[0] - (entity.geometryAttributes as CubeGeometryAttributes).width/2}
+                            onChange={(e) => {
+                              updateParams({
+                                ...entity.geometryAttributes,
+                                width: Math.abs((entity.transformationParams.position[0] + (entity.geometryAttributes as CubeGeometryAttributes).width/2) - parseFloat(e.target.value))
+                            } as CubeGeometryAttributes)
+                            dispatch(updateTransformationParams({...entity.transformationParams, position: [
+                              (parseFloat(e.target.value) + (entity.transformationParams.position[0] + (entity.geometryAttributes as CubeGeometryAttributes).width/2))/2,
+                              entity.transformationParams.position[1],
+                              entity.transformationParams.position[2]]}))
+                            }}
+                      />
+                  </div>
+              </div>
+              <div key="xmax" className="flex">
+                <span className="text-black w-[40%] text-left text-xs">Xmax</span>
+                <div className="flex mb-[5px]">
+                    <input key="xmax"
+                           type="number"
+                           min={entity.transformationParams.position[0] - (entity.geometryAttributes as CubeGeometryAttributes).width/2}
+                           step="0.1"
+                           className="border border-black rounded shadow px-1 w-[50%] text-black text-left text-xs"
+                           autoComplete="off"
+                           value={entity.transformationParams.position[0] + (entity.geometryAttributes as CubeGeometryAttributes).width/2}
+                           onChange={(e) => {
+                            updateParams({
+                              ...entity.geometryAttributes,
+                              width: Math.abs(parseFloat(e.target.value) - (entity.transformationParams.position[0] - (entity.geometryAttributes as CubeGeometryAttributes).width/2))
+                          } as CubeGeometryAttributes)
+                          dispatch(updateTransformationParams({...entity.transformationParams, position: [
+                            (parseFloat(e.target.value) + (entity.transformationParams.position[0] - (entity.geometryAttributes as CubeGeometryAttributes).width/2))/2,
+                            entity.transformationParams.position[1],
+                            entity.transformationParams.position[2]]}))
+                           }}
+                    />
+                </div>
+              </div>
+            </div>
+            <div className="flex row">
+              <div key="ymin" className="flex">
+                  <span className="text-black w-[40%] text-left text-xs">Ymin</span>
+                  <div className="flex mb-[5px]">
+                      <input key="ymin"
+                            type="number"
+                            step="0.1"
+                            max={entity.transformationParams.position[1] + (entity.geometryAttributes as CubeGeometryAttributes).height/2}
+                            className="border border-black rounded shadow px-1 w-[50%] text-black text-left text-xs"
+                            autoComplete="off"
+                            value={entity.transformationParams.position[1] - (entity.geometryAttributes as CubeGeometryAttributes).height/2}
+                            onChange={(e) => {
+                              updateParams({
+                                ...entity.geometryAttributes,
+                                height: Math.abs((entity.transformationParams.position[1] + (entity.geometryAttributes as CubeGeometryAttributes).height/2) - parseFloat(e.target.value))
+                            } as CubeGeometryAttributes)
+                            dispatch(updateTransformationParams({...entity.transformationParams, position: [
+                              entity.transformationParams.position[0],
+                              (parseFloat(e.target.value) + (entity.transformationParams.position[1] + (entity.geometryAttributes as CubeGeometryAttributes).height/2))/2,
+                              entity.transformationParams.position[2]]}))
+                            }}
+                      />
+                  </div>
+              </div>
+              <div key="ymax" className="flex">
+                <span className="text-black w-[40%] text-left text-xs">Ymax</span>
+                <div className="flex mb-[5px]">
+                    <input key="ymax"
+                           type="number"
+                           min={entity.transformationParams.position[1] - (entity.geometryAttributes as CubeGeometryAttributes).height/2}
+                           step="0.1"
+                           className="border border-black rounded shadow px-1 w-[50%] text-black text-left text-xs"
+                           autoComplete="off"
+                           value={entity.transformationParams.position[1] + (entity.geometryAttributes as CubeGeometryAttributes).height/2}
+                           onChange={(e) => {
+                            updateParams({
+                              ...entity.geometryAttributes,
+                              height: Math.abs(parseFloat(e.target.value) - (entity.transformationParams.position[1] - (entity.geometryAttributes as CubeGeometryAttributes).height/2))
+                          } as CubeGeometryAttributes)
+                          dispatch(updateTransformationParams({...entity.transformationParams, position: [
+                            entity.transformationParams.position[0],
+                            (parseFloat(e.target.value) + (entity.transformationParams.position[1] - (entity.geometryAttributes as CubeGeometryAttributes).height/2))/2,
+                            entity.transformationParams.position[2]]}))
+                        }}
+                    />
+                </div>
+              </div>
+            </div>
+            <div className="flex row">
+              <div key="zmin" className="flex">
+                  <span className="text-black w-[40%] text-left text-xs">Zmin</span>
+                  <div className="flex mb-[5px]">
+                      <input key="zmin"
+                            type="number"
+                            max={entity.transformationParams.position[2] + (entity.geometryAttributes as CubeGeometryAttributes).depth/2}
+                            step="0.1"
+                            className="border border-black rounded shadow px-1 w-[50%] text-black text-left text-xs"
+                            autoComplete="off"
+                            value={entity.transformationParams.position[2] - (entity.geometryAttributes as CubeGeometryAttributes).depth/2}
+                            onChange={(e) => {
+                              updateParams({
+                                ...entity.geometryAttributes,
+                                depth: Math.abs((entity.transformationParams.position[2] + (entity.geometryAttributes as CubeGeometryAttributes).depth/2) - parseFloat(e.target.value))
+                            } as CubeGeometryAttributes)
+                            dispatch(updateTransformationParams({...entity.transformationParams, position: [
+                              entity.transformationParams.position[0],
+                              entity.transformationParams.position[1],
+                              (parseFloat(e.target.value) + (entity.transformationParams.position[2] + (entity.geometryAttributes as CubeGeometryAttributes).depth/2))/2
+                              ]}))
+                            }}
+                      />
+                  </div>
+              </div>
+              <div key="zmax" className="flex">
+                <span className="text-black w-[40%] text-left text-xs">Zmax</span>
+                <div className="flex mb-[5px]">
+                    <input key="zmax"
+                           type="number"
+                           min={entity.transformationParams.position[2] - (entity.geometryAttributes as CubeGeometryAttributes).depth/2}
+                           step="0.1"
+                           className="border border-black rounded shadow px-1 w-[50%] text-black text-left text-xs"
+                           autoComplete="off"
+                           value={entity.transformationParams.position[2] + (entity.geometryAttributes as CubeGeometryAttributes).depth/2}
+                           onChange={(e) => {
+                            updateParams({
+                              ...entity.geometryAttributes,
+                              depth: Math.abs(parseFloat(e.target.value) - (entity.transformationParams.position[2] - (entity.geometryAttributes as CubeGeometryAttributes).depth/2))
+                          } as CubeGeometryAttributes)
+                          dispatch(updateTransformationParams({...entity.transformationParams, position: [
+                            entity.transformationParams.position[0],
+                            entity.transformationParams.position[1],
+                            (parseFloat(e.target.value) + (entity.transformationParams.position[2] - (entity.geometryAttributes as CubeGeometryAttributes).depth/2))/2
+                            ]}))
+                           }}
+                    />
+                </div>
+              </div>
+            </div>
             <div key="width" className="flex">
                 <span className="text-black w-[40%] text-left text-xs">width</span>
                 <div className="flex mb-[5px]">
