106875e51c6e75d665066555384b2abaa07a5349
Draco112358 <gianni.frittella@gmail.com>
2024-05-16 01:40:12 +0200

    impostata conversione degli angoli di eulero per la rotazione in modo da inserirla in gradi.

--- src/renderer/canvas/components/sideBar/components/transformations.tsx
+++ src/renderer/canvas/components/sideBar/components/transformations.tsx
@@ -1,4 +1,4 @@
-import {TransformationParams, updateTransformationParams} from "cad-library";
+import {TransformationParamDetails, TransformationParams, updateTransformationParams} from "cad-library";
 import {FC} from "react";
 import {useDispatch} from "react-redux";
 
@@ -14,33 +14,65 @@ export const Transformations: FC<{ transformationParams: TransformationParams }>
                     <div className="text-black w-1/3 text-center font-bold mb-1 text-xs">Z</div>
                 </div>
             </div>
-            {Object.entries(transformationParams).map(([type, value]) =>
-                <div key={type} className="flex justify-between">
-                    <span className="text-black w-[15%] text-xs">{type}</span>
-                    <div className="flex mb-[5px] justify-between pr-[15px] w-[83%]">
-                        {value.map((paramValue, index) =>
-                            <input key={index}
-                                   type="number"
-                                   step="0.1"
-                                   className="border border-black rounded shadow w-[30%] text-black text-xs px-1"
-                                   autoComplete="off"
-                                   value={paramValue}
-                                   onChange={(e) => {
-                                       dispatch(updateTransformationParams(Object.keys(transformationParams).reduce((newTransfParams, typeTransf) => {
-                                           newTransfParams[typeTransf as keyof TransformationParams] = [...newTransfParams[typeTransf as keyof TransformationParams]]
-                                           if (typeTransf === type) {
-                                               newTransfParams[typeTransf as keyof TransformationParams][index] = parseFloat(e.target.value) || 0
-                                           }
-                                           return newTransfParams
-                                       }, {...transformationParams})))
-                                   }}
-                            />
-                        )}
-                    </div>
-                </div>
-            )}
-
+            <div key='position' className="flex justify-between">
+              <span className="text-black w-[15%] text-xs">position</span>
+              <div className="flex mb-[5px] justify-between pr-[15px] w-[83%]">
+                  {transformationParams.position.map((paramValue, index) =>
+                      <input key={index}
+                              type="number"
+                              step="0.1"
+                              className="border border-black rounded shadow w-[30%] text-black text-xs px-1"
+                              autoComplete="off"
+                              value={paramValue}
+                              onChange={(e) => {
+                                let newPosition: TransformationParamDetails = [...transformationParams.position]
+                                newPosition[index] = parseFloat(e.target.value)
+                                dispatch(updateTransformationParams({...transformationParams, position: newPosition}))
+                              }}
+                      />
+                  )}
+              </div>
+            </div>
+            <div key='rotation' className="flex justify-between">
+              <span className="text-black w-[15%] text-xs">rotation [-180°/180°]</span>
+              <div className="flex mb-[5px] justify-between pr-[15px] w-[83%]">
+                  {transformationParams.rotation.map((paramValue, index) =>
+                      <input key={index}
+                              type="number"
+                              step="1"
+                              min={-180}
+                              max={180}
+                              className="border border-black rounded shadow w-[30%] text-black text-xs px-1"
+                              autoComplete="off"
+                              value={(paramValue*180)/Math.PI}
+                              onChange={(e) => {
+                                let newRotation: TransformationParamDetails = [...transformationParams.rotation]
+                                newRotation[index] = (parseFloat(e.target.value)*Math.PI)/180
+                                dispatch(updateTransformationParams({...transformationParams, rotation: newRotation}))
+                              }}
+                      />
+                  )}
+              </div>
+            </div>
+            <div key='scale' className="flex justify-between">
+              <span className="text-black w-[15%] text-xs">scale</span>
+              <div className="flex mb-[5px] justify-between pr-[15px] w-[83%]">
+                  {transformationParams.scale.map((paramValue, index) =>
+                      <input key={index}
+                              type="number"
+                              step="0.1"
+                              className="border border-black rounded shadow w-[30%] text-black text-xs px-1"
+                              autoComplete="off"
+                              value={paramValue}
+                              onChange={(e) => {
+                                let newScale: TransformationParamDetails = [...transformationParams.scale]
+                                newScale[index] = parseFloat(e.target.value)
+                                dispatch(updateTransformationParams({...transformationParams, scale: newScale}))
+                              }}
+                      />
+                  )}
+              </div>
+            </div>
         </>
-
     )
 }
